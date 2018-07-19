8e36ed408330e5a153570e770a376d6779a706ee
u6bkep <u6bkep@gmail.com>
2018-07-19 09:32:53 -0400

    ported print example to raspberry pi C++

---
+++ examples/print/print.cpp
@@ -0,0 +1,36 @@
+#include <LedDisplayPi.h>
+#include <chrono>
+
+// Define pins for the LED display.
+// You can change these, just re-wire your board:
+#define dataPin 6              // connects to the display's data in
+#define registerSelect 7       // the display's register select pin
+#define clockPin 8             // the display's clock pin
+#define enable 9               // the display's chip enable pin
+#define reset 10              // the display's reset pin
+
+#define displayLength 8        // number of characters in the display
+
+void main(void)
+{
+	// create am instance of the LED display library:
+	LedDisplay myDisplay = LedDisplay(dataPin, registerSelect, clockPin,
+                                      enable, reset, displayLength);
+	int brightness = 15;        // screen brightness
+
+	// initialize the display library:
+	myDisplay.begin();
+	// set the brightness of the display:
+	myDisplay.setBrightness(brightness);
+
+	while(1)
+	{
+		// set cursor to 0
+		myDisplay.home();
+		// print the time
+		myDisplay.print(std::chrono::system_clock::now());
+	}
+
+
+	return 0;
+}
