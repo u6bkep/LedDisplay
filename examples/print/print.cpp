db6cbfc875cd3b2c1a686b4ff351f16c3080990d
u6bkep <u6bkep@gmail.com>
2018-08-15 17:47:09 +0000

    got example to compile, not we need to make the linker happy

--- examples/print/print.cpp
+++ examples/print/print.cpp
@@ -15,9 +15,9 @@ int main(void)
 {
 	char helloWorldArray[] = "Hello world!";
 	// create am instance of the LED display library:
-	LedDisplay::LedDisplay myDisplay;
-	myDisplay.LedDisplay(dataPin, registerSelect, clockPin,
-                         enable, reset, displayLength);
+
+	LedDisplay::LedDisplay myDisplay(dataPin, registerSelect, clockPin, enable, reset, displayLength);
+
 	int brightness = 15;        // screen brightness
 
 	// initialize the display library:
